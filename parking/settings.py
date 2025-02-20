d13cf5b6614d1984060d60956c2a8f6f4b7eb079
pterro2020 <154668100+pterro2020@users.noreply.github.com>
2025-02-20 22:34:35 +0300

    Update settings.py

--- parking/settings.py
+++ parking/settings.py
@@ -90,8 +90,8 @@ WSGI_APPLICATION = 'parking.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': 'db.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'db.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
